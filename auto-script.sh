c143dd126a9d43ee700579419d6cecec519acee0
Vinh Do Ngoc <ngocvinh.evin227@gmail.com>
2025-01-15 14:30:36 +0700

    Create auto-script.sh

---
+++ auto-script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export APPROVE_INSTALL=y
+export APPROVE_IP=y
+export IPV6_SUPPORT=n
+export PORT_CHOICE=1
+export PROTOCOL_CHOICE=1
+export DNS=1
+export COMPRESSION_ENABLED=n
+export CUSTOMIZE_ENC=n
+export CLIENT="hvtvpn"
+./openvpn-install.sh
