b54749e40928dd4511b03522276713619592c2f3
Vinh Do Ngoc <ngocvinh.evin227@gmail.com>
2025-01-15 14:26:15 +0700

    Update openvpn-install.sh enable multipe connection by one client

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -933,7 +933,9 @@ tls-version-min 1.2
 tls-cipher $CC_CIPHER
 client-config-dir /etc/openvpn/ccd
 status /var/log/openvpn/status.log
-verb 3" >>/etc/openvpn/server.conf
+verb 3
+duplicate-cn
+" >>/etc/openvpn/server.conf
 
 	# Create client-config-dir dir
 	mkdir -p /etc/openvpn/ccd
